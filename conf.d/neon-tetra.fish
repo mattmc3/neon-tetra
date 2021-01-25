dbd5f24f34698c5f47113aab89d9520aa91d2eb9
mattmc3 <mattmc3@gmail.com>
2021-01-25 11:11:01 -0500

    Add git dirty color

--- conf.d/neon-tetra.fish
+++ conf.d/neon-tetra.fish
@@ -4,6 +4,7 @@ if test "$neon_tetra_initialized" != true
     set -U pure_symbol_reverse_prompt "«"
     set -U pure_color_success cyan
     set -U pure_color_info magenta
+    set -U pure_color_git_dirty magenta
 
     if not functions -q fisher
         curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
