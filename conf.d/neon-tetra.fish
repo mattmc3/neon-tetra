d3c8bf50fb435cce70ae295889489a513578881b
mattmc3 <mattmc3@gmail.com>
2021-03-16 09:52:09 -0400

    Fix pure dependency

--- conf.d/neon-tetra.fish
+++ conf.d/neon-tetra.fish
@@ -5,18 +5,18 @@ if test "$neon_tetra_initialized" != true
     set -U pure_color_success cyan
     set -U pure_color_info magenta
     set -U pure_color_git_dirty magenta
+end
 
-    if not functions -q _pure_prompt
-        set -l purepath (realpath (status --current-filename)/../../modules/pure)
-        if test -d $purepath/completions; and not contains $purepath/completions $fish_complete_path
-            set fish_complete_path $fish_complete_path[1] $purepath/completions $fish_complete_path[2..-1]
-        end
-        if test -d $purepath/functions; and not contains $purepath/functions $fish_function_path
-            set fish_function_path $fish_function_path[1] $purepath/functions $fish_function_path[2..-1]
-        end
-        for f in $plugin/conf.d/*.fish
-            builtin source "$f"
-        end
+if not functions -q _pure_prompt
+    set -l purepath (realpath (status --current-filename)/../../modules/pure)
+    if test -d $purepath/completions; and not contains $purepath/completions $fish_complete_path
+        set fish_complete_path $fish_complete_path[1] $purepath/completions $fish_complete_path[2..-1]
+    end
+    if test -d $purepath/functions; and not contains $purepath/functions $fish_function_path
+        set fish_function_path $fish_function_path[1] $purepath/functions $fish_function_path[2..-1]
+    end
+    for f in $plugin/conf.d/*.fish
+        builtin source "$f"
     end
 end
 
